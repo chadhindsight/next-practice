8199c4b8496e2c98a1f9f56d0cf5e77517f53f50
chadhindsight <chadrickJ8@gmail.com>
2025-09-16 15:02:33 -0400

    Adjust comment to be more descripttive

--- app/nova/layout.tsx
+++ app/nova/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html>
       <body>
         <nav>
-          {/* Prefetched when the link is hovered or enters the viewport */}
+          {/* Prefetched when the link is hovered or enters the viewport. */}
           <Link href="/blog">Blog</Link>
           <a href="/contact">Contact</a>
         </nav>
